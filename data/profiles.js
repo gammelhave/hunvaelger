5baa56e8dd2d93506cedf84950ced8ac9de1782d
gammelhave <gammelhave@gmail.com>
2025-10-23 15:59:01 +0200

    Create profiles.js

---
+++ data/profiles.js
@@ -0,0 +1,20 @@
+export const profiles = [
+  {
+    id: "a1",
+    name: "Anna",
+    age: 27,
+    city: "Sønderborg",
+    interests: ["heste", "musik", "outdoor"],
+    photo: "/avatars/anna.jpg",
+    bio: "Elsker lange gåture og kaffe på havnen."
+  },
+  {
+    id: "b2",
+    name: "Mads",
+    age: 31,
+    city: "Nordborg",
+    interests: ["gaming", "madlavning", "rejser"],
+    photo: "/avatars/mads.jpg",
+    bio: "Kan lave en killer carbonara."
+  }
+];
