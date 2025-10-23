29aae8a45915c93a17f3ad059537c8c918ad386b
gammelhave <gammelhave@gmail.com>
2025-10-23 15:11:26 +0200

    Delete pages/api directory

--- pages/api/health.js
+++
@@ -1,5 +0,0 @@
-// pages/api/health.js
-export default function handler(req, res) {
-  res.setHeader('content-type', 'application/json');
-  res.status(200).send(JSON.stringify({ ok: true, route: '/api/health' }));
-}
