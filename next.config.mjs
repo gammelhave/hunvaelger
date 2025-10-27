72435cf58a9cf7f673a0942765fa02f109e8cc8e
gammelhave <gammelhave@gmail.com>
2025-10-27 08:56:04 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,19 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=()" }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
