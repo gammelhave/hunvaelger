1375cc6e1dd4a310ce21866e7695f99f66a92d09
gammelhave <gammelhave@gmail.com>
2025-10-27 08:00:16 +0100

    Update route.js

--- app/robots.txt/route.js
+++ app/robots.txt/route.js
@@ -1,6 +1,10 @@
 export async function GET() {
   const body = `User-agent: *
 Disallow: /admin
+Disallow: /api
+Allow: /
+
+Sitemap: https://hunvaelger.vercel.app/sitemap.xml
 `;
   return new Response(body, { headers: { "Content-Type": "text/plain" } });
 }
