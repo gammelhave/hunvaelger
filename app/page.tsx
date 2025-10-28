1508818b45861d0dd3f40783de841dd327a13f1d
gammelhave <gammelhave@gmail.com>
2025-10-28 10:29:15 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-"use client"
-
+// app/page.tsx
 import Hero from "@/components/Hero"
 import { AboutTeaser } from "@/components/AboutTeaser"
+import FAQ from "@/components/FAQ"
 
 export default function HomePage() {
   return (
     <main className="flex flex-col">
       <Hero />
       <AboutTeaser />
+      <FAQ />
     </main>
   )
 }
