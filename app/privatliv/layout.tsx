40397188d08975d8b61180912daeefa2b4a7733b
gammelhave <gammelhave@gmail.com>
2025-10-28 09:34:34 +0100

    Create layout.tsx

---
+++ app/privatliv/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Privatlivspolitik",
+  description:
+    "Læs hvordan HunVælger behandler og beskytter dine personoplysninger.",
+}
+
+export default function PrivatlivLayout({ children }: { children: React.ReactNode }) {
+  return children
+}
